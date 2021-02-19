3782125366ccf876fc82608cba7f000d605520fa
Taha <Tahasdesktop@gmail.com>
2021-02-19 18:09:32 +0500

    If-else Statement

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-name = input ("Enter your name:")
-print(f"Hello,{name}")
+# name = input ("Enter your name:")
+# print(f"Hello,{name}")
+
+n = int(input("Number:"))
+
+if n > 0:
+    print("n is +ve")
+elif n < 0:
+    print("n is -ve")
+else:
+    print("n is xero")
+    
