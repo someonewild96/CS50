1fa856a2bf4680a1c2b0f568a14af99f4aa6592f
Taha <Tahasdesktop@gmail.com>
2021-02-20 20:36:43 +0500

    Added Functions

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
 # name = input ("Enter your name:")
 # print(f"Hello,{name}")
 
-n = int(input("Number:"))
+# n = int(input("Number:"))
 
-if n > 0:
-    print("n is +ve")
-elif n < 0:
-    print("n is -ve")
-else:
-    print("n is xero")
-    
+# if n > 0:
+#     print("n is +ve")
+# elif n < 0:
+#     print("n is -ve")
+# else:
+#     print("n is xero")
+
+# Functions
+
+def square(x):
+    s = x * x
+    return s
